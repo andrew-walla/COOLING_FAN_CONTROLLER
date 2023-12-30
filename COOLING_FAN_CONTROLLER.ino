8b88ad56bb500fb78fd9a0bfd06c70c817dc29d6
andrew-walla <86935859+andrew-walla@users.noreply.github.com>
2023-12-30 13:56:46 +1100

    Update COOLING_FAN_CONTROLLER.ino
    Revisted algorithm for PwmCounter. Put compile time constants to enable/disable sensor inputs. Smarter logic for calculating relay current failure condition.

--- COOLING_FAN_CONTROLLER.ino
+++ COOLING_FAN_CONTROLLER.ino
@@ -15,8 +15,8 @@ const double  TO_KELVIN = 273.15;
 const double  TO_CELSIUS = -TO_KELVIN;
 
 // Constants
-const bool    ON = LOW;   // Fan ONOFF control
 const bool    OFF = HIGH; // Fan ONOFF control
+const bool    ON = LOW;   // Fan ONOFF control
 const bool    HIGH_SPEED = LOW; // Fan speed control
 const bool    LOW_SPEED = HIGH; // Fan speed control
 const bool    NO_FAILURE_DETECTED = HIGH; // Failsafe control
@@ -27,9 +27,13 @@ const double  INTERNAL_THERMISTOR_B_CONSTANT = 3455*K;
 const double  EXTERNAL_THERMISTOR_B_CONSTANT = 3455*K; // TODO: Empirically work this out
 const bool    ENABLE_DIAGNOSTICS = true;
 const double  DIAGNOSTICS_PRINT_INTERVAL = 1.0*s;
-const double  PWM_FREQUENCY = 10*kHz; // TODO: Empirically work this out
+const double  FAN_CONTROLLER_SAMPLE_INTERVAL = 1.0*s;
 const double  RELAY_CURRENT = 10*mA;  // TODO: Empirically work this out
-const bool    ENABLE_TEMPERATURE_SENSOR = true;
+const bool    ENABLE_EXTERNAL_TEMPERATURE_SENSOR = false;
+const bool    ENABLE_PCB_TEMPERATURE_SENSOR = true;
+const bool    ENABLE_DIGITAL_FAN_SENSOR = true;
+const bool    ENABLE_ANALOG_FAN_SENSOR = false;
+const long    BAUD = 57600;
 
 // Analog Inputs
 const int     PSU_3V3 = A0;
@@ -157,47 +161,40 @@ private:
 
 class PwmCounter {
 public:
-  PwmCounter(const unsigned pin, const double frequency) :
+  PwmCounter(const unsigned pin, const double sample_time) :
     _pin(pin),
-    _period_us((1/frequency)*TO_MICROSECONDS),
-    _last_high(0),
-    _this_high(0),
-    _last_low(0),
-    _last_state(LOW) {
+    _sample_us(sample_time*TO_MICROSECONDS),
+    _high_count(0),
+    _total_count(0),
+    _percent(0),
+    _last_reset(0) {
   }
 
   void update() {
-    bool this_state = digitalRead(_pin);
-    if(this_state==_last_state)
+    if(digitalRead(_pin))
+      _high_count++;
+    _total_count++;
+    unsigned long now = micros();
+    if(now-_last_reset<_sample_us)
       return;
-    switch(this_state) {
-      case HIGH:
-        _last_high = _this_high;
-        _this_high = micros();
-      case LOW:
-        _last_low = micros();
-    }
-    _last_state = this_state;
+    _percent = 100.0*_high_count/_total_count;
+    _last_reset = now;
+    _high_count = 0;
+    _total_count = 0;
   }
 
   operator double() {
     update();
-    if(_this_high==0)
-      return 0.0;
-    if(micros()-_last_high>_period_us)
-      return digitalRead(_pin)?100.0:0.0;
-    double period = _this_high-_last_high;
-    double duty = _last_low>_last_high?_last_low-_last_high:0.0;
-    return (1.0-duty/period)*100.0;
+    return _percent;
   }
 
 private:
   const unsigned _pin;
-  const unsigned _period_us;
-  unsigned long _last_high;
-  unsigned long _last_low;
-  unsigned long _this_high;
-  bool _last_state;
+  const unsigned _sample_us;
+  unsigned _high_count;
+  unsigned _total_count;
+  double _percent;
+  unsigned long _last_reset;
 };
 
 // Objects
@@ -205,100 +202,127 @@ private:
 State* state;
 Timer state_machine_timer(STATE_MACHINE_TICK);
 Timer diagnostics_timer(DIAGNOSTICS_PRINT_INTERVAL);
-PwmCounter fan_speed_digital(FAN_SPEED_D, PWM_FREQUENCY);
+PwmCounter fan_speed_digital(FAN_SPEED_D, FAN_CONTROLLER_SAMPLE_INTERVAL);
 
 // States of operation
 
 State* engine_cold() {
+  blink_period = 1.0*s;
   digitalWrite(FAILSAFE_ENABLE, NO_FAILURE_DETECTED);
   digitalWrite(ONOFF_ENABLE, OFF);
   digitalWrite(FAN_SPEED, LOW_SPEED);
-  blink_period = 1.0*s;
+  double expected_current = (OFF?RELAY_CURRENT:0)+(LOW_SPEED?RELAY_CURRENT:0);
 
-  if(fan_speed_digital>50.0*PERCENT)
-    return engine_warm;
+  if(ENABLE_DIGITAL_FAN_SENSOR)
+    if(fan_speed_digital>50.0*PERCENT)
+      return engine_warm;
 
-  if(fan_speed_analogue()>50.0*PERCENT)
-    return engine_warm;
+  if(ENABLE_ANALOG_FAN_SENSOR)
+    if(fan_speed_analogue()>50.0*PERCENT)
+      return engine_warm;
     
-  if(ENABLE_TEMPERATURE_SENSOR)
+  if(ENABLE_EXTERNAL_TEMPERATURE_SENSOR)
     if(engine_temperature()>80.0*C)
       return engine_warm;
 
-  if(pcb_temperature()>50.0*C)
-    return engine_warm;
+  if(ENABLE_PCB_TEMPERATURE_SENSOR)
+    if(pcb_temperature()>50.0*C)
+      return engine_warm;
   
-  if(switch_current()<RELAY_CURRENT/10)
+  if(switch_current()>expected_current+RELAY_CURRENT/2)
     return failure_detected;
 
-    return engine_cold;
+  if(switch_current()<expected_current-RELAY_CURRENT/2)
+    return failure_detected;
+
+  return engine_cold;
 }
 
 State* engine_warm() {
+  blink_period = 1.0*s;
   digitalWrite(FAILSAFE_ENABLE, NO_FAILURE_DETECTED);
   digitalWrite(ONOFF_ENABLE, ON);
   digitalWrite(FAN_SPEED, LOW_SPEED);
-  blink_period = 1.0*s;
+  double expected_current = (ON?RELAY_CURRENT:0)+(LOW_SPEED?RELAY_CURRENT:0);
 
-  if(fan_speed_digital>80.0*PERCENT)
-    return engine_hot;
+  if(ENABLE_DIGITAL_FAN_SENSOR)
+    if(fan_speed_digital>80.0*PERCENT)
+      return engine_hot;
 
-  if(fan_speed_analogue()>80.0*PERCENT)
-    return engine_hot;
+  if(ENABLE_ANALOG_FAN_SENSOR)
+    if(fan_speed_analogue()>80.0*PERCENT)
+      return engine_hot;
     
-  if(ENABLE_TEMPERATURE_SENSOR)
+  if(ENABLE_EXTERNAL_TEMPERATURE_SENSOR)
     if(engine_temperature()>95.0*C)
       return engine_hot;
 
-  if(pcb_temperature()>80.0*C)
-    return engine_hot;
+  if(ENABLE_PCB_TEMPERATURE_SENSOR)
+    if(pcb_temperature()>80.0*C)
+      return engine_hot;
 
-  if(fan_speed_digital<40.0*PERCENT)
-    return engine_cold;
+  if(ENABLE_DIGITAL_FAN_SENSOR)
+    if(fan_speed_digital<40.0*PERCENT)
+      return engine_cold;
 
-  if(fan_speed_analogue()<40.0*PERCENT)
-    return engine_cold;
+  if(ENABLE_ANALOG_FAN_SENSOR)
+    if(fan_speed_analogue()<40.0*PERCENT)
+      return engine_cold;
 
-  if(ENABLE_TEMPERATURE_SENSOR)
+  if(ENABLE_EXTERNAL_TEMPERATURE_SENSOR)
     if(engine_temperature()<30.0*C)
       return engine_cold;
 
-  if(pcb_temperature()<10.0*C)
-    return engine_cold;
+  if(ENABLE_PCB_TEMPERATURE_SENSOR)
+    if(pcb_temperature()<10.0*C)
+      return engine_cold;
+
+  if(switch_current()>expected_current+RELAY_CURRENT/2)
+    return failure_detected;
+
+  if(switch_current()<expected_current-RELAY_CURRENT/2)
+    return failure_detected;
 
   return engine_warm;
 }
 
 State* engine_hot() {
+  blink_period = 1.0*s;
   digitalWrite(FAILSAFE_ENABLE, NO_FAILURE_DETECTED);
   digitalWrite(ONOFF_ENABLE, ON);
   digitalWrite(FAN_SPEED, HIGH_SPEED);
-  blink_period = 1.0*s;
+  double expected_current = (ON?RELAY_CURRENT:0)+(HIGH_SPEED?RELAY_CURRENT:0);
 
-  if(fan_speed_digital<70.0*PERCENT)
-    return engine_warm;
+  if(ENABLE_DIGITAL_FAN_SENSOR)
+    if(fan_speed_digital<70.0*PERCENT)
+      return engine_warm;
 
-  if(fan_speed_analogue()<70.0*PERCENT)
-    return engine_warm;
+  if(ENABLE_ANALOG_FAN_SENSOR)
+    if(fan_speed_analogue()<70.0*PERCENT)
+      return engine_warm;
     
-  if(ENABLE_TEMPERATURE_SENSOR)
+  if(ENABLE_EXTERNAL_TEMPERATURE_SENSOR)
     if(engine_temperature()<90.0*C)
       return engine_warm;
 
-  if(pcb_temperature()<70.0*C)
-    return engine_warm;
+  if(ENABLE_PCB_TEMPERATURE_SENSOR)
+    if(pcb_temperature()<70.0*C)
+      return engine_warm;
 
-  if(switch_current()>RELAY_CURRENT/2)
+  if(switch_current()>expected_current+RELAY_CURRENT/2)
+    return failure_detected;
+
+  if(switch_current()<expected_current-RELAY_CURRENT/2)
     return failure_detected;
 
   return engine_hot;
 }
 
 State* failure_detected() {
+  blink_period = 0.25*s;
   digitalWrite(FAILSAFE_ENABLE, FAILURE_DETECTED);
   digitalWrite(ONOFF_ENABLE, ON);
   digitalWrite(FAN_SPEED, HIGH_SPEED);
-  blink_period = 0.25*s;
   return failure_detected;
 }
 
@@ -325,6 +349,16 @@ void diagnostics() {
   Serial.println("EXTERNAL_TEMPERATURE\t"+String(engine_temperature()));
   Serial.println("FAN_SPEED_D\t"+String((double)fan_speed_digital));
   Serial.println("FAN_SPEED_A\t"+String(fan_speed_analogue()));
+  if(state==engine_cold)
+    Serial.println("STATE\t\tENGINE_COLD");
+  else if(state==engine_warm)
+    Serial.println("STATE\t\tENGINE_WARM");
+  else if(state==engine_hot)
+    Serial.println("STATE\t\tENGINE_HOT");
+  else if(state==failure_detected)
+    Serial.println("STATE\t\tFAILURE_DETECTED");
+  else
+    Serial.println("STATE\t\tUNKNOWN");
 }
 
 void setup() {
@@ -339,7 +373,7 @@ void setup() {
   state = engine_cold;
   blink_period = 1.0*s;
   if(ENABLE_DIAGNOSTICS)
-    Serial.begin(57600);
+    Serial.begin(BAUD);
 }
 
 void loop() {
